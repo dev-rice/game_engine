2d2427c354f4d69ee25ea92db6a61b9ad76fe00b
Chris Rice <chrisrice93@gmail.com>
2016-11-12 16:22:46 -0700

    Working sprite drawer

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"runtime"
 
 	"github.com/donutmonger/game_engine/shader"
-	"github.com/donutmonger/game_engine/sprite"
 	"github.com/donutmonger/game_engine/texture"
 	"github.com/donutmonger/game_engine/window"
 
@@ -60,34 +59,13 @@ func main() {
 	}
 	shaderProgram.Use()
 
-	projection := mgl32.Perspective(mgl32.DegToRad(45.0), window.AspectRatio(), 0.1, 10.0)
-	projectionUniform := shaderProgram.GetUniformLocation("projection")
-	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
-
-	view := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
-	viewUniform := shaderProgram.GetUniformLocation("view")
-	gl.UniformMatrix4fv(viewUniform, 1, false, &view[0])
-
-	model := mgl32.Ident4()
-	modelUniform := shaderProgram.GetUniformLocation("model")
-	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
-
-	textureUniform := shaderProgram.GetUniformLocation("tex")
-	gl.Uniform1i(textureUniform, 0)
-
 	gl.BindFragDataLocation(shaderProgram.GLid, 0, gl.Str("outputColor\x00"))
 
 	// Load the texture
-	stone_texture, err := texture.NewTextureFromFile("stone.png")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	player_texture, err := texture.NewTextureFromFile("enemy1.png")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	player_sprite := sprite.NewSprite(*player_texture)
 
 	// Configure the vertex data
 	var vao uint32
@@ -97,52 +75,36 @@ func main() {
 	var vbo uint32
 	gl.GenBuffers(1, &vbo)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(gl.ARRAY_BUFFER, len(cubeVertices)*4, gl.Ptr(cubeVertices), gl.STATIC_DRAW)
+	gl.BufferData(gl.ARRAY_BUFFER, len(flatMeshVertices)*4, gl.Ptr(flatMeshVertices), gl.STATIC_DRAW)
 
-	var ebo uint32
-	gl.GenBuffers(1, &ebo)
-	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
-	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(cubeElements)*4, gl.Ptr(cubeElements), gl.STATIC_DRAW)
+	posAttrib := uint32(shaderProgram.GetAttribLocation("position"))
+	gl.EnableVertexAttribArray(posAttrib)
+	gl.VertexAttribPointer(posAttrib, 2, gl.FLOAT, false, 4*4, gl.PtrOffset(0))
 
-	vertAttrib := uint32(shaderProgram.GetAttribLocation("vert"))
-	gl.EnableVertexAttribArray(vertAttrib)
-	gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
-
-	texCoordAttrib := uint32(shaderProgram.GetAttribLocation("vertTexCoord\x00"))
-	gl.EnableVertexAttribArray(texCoordAttrib)
-	gl.VertexAttribPointer(texCoordAttrib, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
+	texcoordAttrib := uint32(shaderProgram.GetAttribLocation("texcoord"))
+	gl.EnableVertexAttribArray(texcoordAttrib)
+	gl.VertexAttribPointer(texcoordAttrib, 2, gl.FLOAT, false, 4*4, gl.PtrOffset(2*4))
 
 	// Configure global settings
-	gl.Enable(gl.DEPTH_TEST)
-	gl.DepthFunc(gl.LESS)
-	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
-
-	angle := 0.0
-	previousTime := glfw.GetTime()
+	gl.Disable(gl.DEPTH_TEST)
+	gl.Disable(gl.CULL_FACE)
+	gl.ClearColor(0.2, 0.2, 0.8, 0.0)
 
 	for !window.GlfwWindow.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		// Update
-		time := glfw.GetTime()
-		elapsed := time - previousTime
-		previousTime = time
-
-		angle += elapsed
-		model = mgl32.HomogRotate3D(float32(angle), mgl32.Vec3{0, 1, 0.1})
-
 		// Render
 		shaderProgram.Use()
-		gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
-
 		gl.BindVertexArray(vao)
 
+		model := mgl32.Ident3()
+		modelUniform := shaderProgram.GetUniformLocation("transformation")
+		gl.UniformMatrix3fv(modelUniform, 1, false, &model[0])
+
 		gl.ActiveTexture(gl.TEXTURE0)
-		stone_texture.Bind2D()
+		player_texture.Bind2D()
 
-		player_sprite.Draw()
-
-		gl.DrawElements(gl.TRIANGLES, 36, gl.UNSIGNED_INT, gl.PtrOffset(0))
+		gl.DrawArrays(gl.TRIANGLES, 0, 6)
 
 		// Maintenance
 		window.GlfwWindow.SwapBuffers()
@@ -153,82 +115,43 @@ func main() {
 var vertexShaderSource = `
 #version 330
 
-uniform mat4 projection;
-uniform mat4 view;
-uniform mat4 model;
+in vec2 position;
+in vec2 texcoord;
 
-in vec3 vert;
-in vec2 vertTexCoord;
+out vec2 Texcoord;
 
-out vec2 fragTexCoord;
+uniform mat3 transformation;
 
 void main() {
-    fragTexCoord = vertTexCoord;
-    gl_Position = projection * view * model * vec4(vert, 1);
+    Texcoord = vec2(texcoord.x, 1 - texcoord.y);
+    vec3 position_temp = vec3(position, 1.0) * transformation;
+    gl_Position = vec4(position_temp.xy, 0.0, 1.0);
 }
 ` + "\x00"
 
 var fragmentShaderSource = `
 #version 330
 
-uniform sampler2D tex;
-
-in vec2 fragTexCoord;
+in vec2 Texcoord;
 
 out vec4 outputColor;
 
+uniform float opacity;
+uniform sampler2D base_texture;
+
 void main() {
-    outputColor = texture(tex, fragTexCoord);
+    vec4 texel = textureOffset(base_texture, Texcoord, ivec2(-0.5 , -0.5));
+
+    outputColor = vec4(texel.rgb, opacity * texel.a);
 }
 ` + "\x00"
 
-var cubeVertices = []float32{
-	-0.5, -0.5, -0.5, 0.0, 0.0,
-	0.5, -0.5, -0.5, 1.0, 0.0,
-	0.5, 0.5, -0.5, 1.0, 1.0,
-	-0.5, 0.5, -0.5, 0.0, 1.0,
+var flatMeshVertices = []float32{
+	-1.0, 1.0, 0.0, 1.0,
+	1.0, 1.0, 1.0, 1.0,
+	1.0, -1.0, 1.0, 0.0,
 
-	-0.5, -0.5, 0.5, 0.0, 0.0,
-	0.5, -0.5, 0.5, 1.0, 0.0,
-	0.5, 0.5, 0.5, 1.0, 1.0,
-	-0.5, 0.5, 0.5, 0.0, 1.0,
-
-	-0.5, 0.5, 0.5, 1.0, 0.0,
-	-0.5, 0.5, -0.5, 1.0, 1.0,
-	-0.5, -0.5, -0.5, 0.0, 1.0,
-	-0.5, -0.5, 0.5, 0.0, 0.0,
-
-	0.5, 0.5, 0.5, 1.0, 0.0,
-	0.5, 0.5, -0.5, 1.0, 1.0,
-	0.5, -0.5, -0.5, 0.0, 1.0,
-	0.5, -0.5, 0.5, 0.0, 0.0,
-
-	-0.5, -0.5, -0.5, 0.0, 1.0,
-	0.5, -0.5, -0.5, 1.0, 1.0,
-	0.5, -0.5, 0.5, 1.0, 0.0,
-	-0.5, -0.5, 0.5, 0.0, 0.0,
-
-	-0.5, 0.5, -0.5, 0.0, 1.0,
-	0.5, 0.5, -0.5, 1.0, 1.0,
-	0.5, 0.5, 0.5, 1.0, 0.0,
-	-0.5, 0.5, 0.5, 0.0, 0.0,
-}
-var cubeElements = []uint32{
-	0, 1, 3,
-	1, 2, 3,
-
-	4, 5, 7,
-	5, 6, 7,
-
-	8, 9, 11,
-	9, 10, 11,
-
-	12, 13, 15,
-	13, 14, 15,
-
-	16, 17, 19,
-	17, 18, 19,
-
-	20, 21, 23,
-	21, 22, 23,
+	1.0, -1.0, 1.0, 0.0,
+	-1.0, -1.0, 0.0, 0.0,
+	-1.0, 1.0, 0.0, 1.0,
 }
